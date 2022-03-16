9141a5bb3447a042e3d665ce85ceafe1c30a7134
Mikhail <misha.konopkin@gmail.com>
2022-03-16 19:23:30 +0300

    Добавьте файлы проекта.

---
+++ powerPoint/PresentationSeparator.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace powerPoint
+{
+    class PresentationSeparator
+    {
+        public Image[] ConvertToPictures(string path)
+        {
+            var ppApp = new Microsoft.Office.Interop.PowerPoint.Application();
+
+            ppApp.Visible = MsoTriState.msoCTrue;
+            Presentations ppPresens = ppApp.Presentations;
+            Presentation objPres = ppPresens.Open(@"C:\work\test.pptx",
+                MsoTriState.msoTrue, MsoTriState.msoFalse, MsoTriState.msoFalse);
+
+            Image[] images = new Bitmap[objPres.Slides.Count];
+            for (int i = 1; i <= objPres.Slides.Count; i++)
+            {
+                objPres.Slides[i].Export($"C:\\work\\slides\\NEWNAME{i}.png", "png", 1280, 720);
+                images[i - 1] = new Bitmap($"C:\\work\\slides\\NEWNAME{i}.png");
+            }
+
+            objPres.Close();
+            objPres = null;
+            ppApp.Quit();
+            ppApp = null;
+
+            return images;
+        }
+
+        public Image[] ConvertToCompressedPictures(string path)
+        {
+            var ppApp = new Microsoft.Office.Interop.PowerPoint.Application();
+
+            ppApp.Visible = MsoTriState.msoCTrue;
+            Presentations ppPresens = ppApp.Presentations;
+            Presentation objPres = ppPresens.Open(@"C:\work\test.pptx",
+                MsoTriState.msoTrue, MsoTriState.msoFalse, MsoTriState.msoFalse);
+
+            Image[] compressedImages = new Bitmap[objPres.Slides.Count];
+            for (int i = 1; i <= objPres.Slides.Count; i++)
+            {
+                objPres.Slides[i].Export($"C:\\work\\slides\\comprasedNEWNAME{i}.png", "png", 320, 240);
+                compressedImages[i - 1] = new Bitmap($"C:\\work\\slides\\comprasedNEWNAME{i}.png");
+            }
+
+            objPres.Close();
+            objPres = null;
+            ppApp.Quit();
+            ppApp = null;
+
+            return compressedImages;
+        }
+
+    }
+}
---
+++ powerPoint/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Core;
+using System.Drawing;
+
+namespace powerPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "";
+            PresentationSeparator presentationSeparator = new PresentationSeparator();
+            presentationSeparator.ConvertToPictures(path);
+        }
+    }
+}
